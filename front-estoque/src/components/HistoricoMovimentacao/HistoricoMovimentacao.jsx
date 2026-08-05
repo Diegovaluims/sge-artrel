2ec5642f35ddd9d11aaebb394f9056d5194425ce
Diegovaluims <diegoluisvalim@icloud.com>
2026-08-05 12:34:26 -0300

    feat: added tab 'Histórico' with placeholder for future development

---
+++ front-estoque/src/components/HistoricoMovimentacao/HistoricoMovimentacao.jsx
@@ -0,0 +1,12 @@
+// HistoricoMovimentacao.jsx
+// Componente para exibir o histórico de movimentações.
+
+export default function HistoricoMovimentacao() {
+    
+    
+    return (
+    <div>
+      <h2>Histórico de Movimentação</h2>
+    </div>
+  );
+}
