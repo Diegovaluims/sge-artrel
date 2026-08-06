2d6b6387d6da3ac4ee6811191fa6eac9da241d8d
Diegovaluims <diegoluisvalim@icloud.com>
2026-08-06 08:44:10 -0300

    chore: configuring cors and environment variable to test system remotly

--- front-estoque/src/config.js
+++ front-estoque/src/config.js
@@ -1,4 +1,4 @@
 // src/config.js
 // Ponto único de configuração da API.
 // Altere POSTGREST_URL se a porta ou host do PostgREST mudar.
-export const POSTGREST_URL = 'http://localhost:3000';
+export const POSTGREST_URL = import.meta.env.VITE_POSTGREST_URL || 'http://localhost:3000';
